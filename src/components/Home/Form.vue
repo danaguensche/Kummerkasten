d3ba69d57805271a559b9c9e178cde603ed84585
danaguensche <dana.guensche@ausbildung.diakonie-am-campus.de>
2025-09-23 14:58:50 +0200

    ui test

--- src/components/Home/Form.vue
+++ src/components/Home/Form.vue
@@ -5,131 +5,77 @@
                 Dein Anliegen
             </v-card-title>
             <v-divider class="mb-3"></v-divider>
-        
-            
+
+
             <v-card-text class="pa-0">
                 <v-form ref="form" v-model="valid" @submit.prevent="submitForm">
-                    <v-text-field 
-                        v-model="formData.firstname" 
-                        label="Vorname (optional)" 
-                        variant="outlined"
-                        density="compact" 
-                        prepend-inner-icon="mdi-account" 
-                        class="mb-3" 
-                        color="indigo-darken-2"
+                    <v-text-field v-model="formData.firstname" label="Vorname (optional)" variant="outlined"
+                        density="compact" prepend-inner-icon="mdi-account" class="mb-3" color="indigo-darken-2"
                         :disabled="loading">
                     </v-text-field>
-                    
-                    <v-text-field 
-                        v-model="formData.lastname" 
-                        label="Nachname (optional)" 
-                        variant="outlined"
-                        density="compact" 
-                        prepend-inner-icon="mdi-account" 
-                        class="mb-3" 
-                        color="indigo-darken-2"
+
+                    <v-text-field v-model="formData.lastname" label="Nachname (optional)" variant="outlined"
+                        density="compact" prepend-inner-icon="mdi-account" class="mb-3" color="indigo-darken-2"
                         :disabled="loading">
                     </v-text-field>
-                    
-                    <v-select 
-                        v-model="formData.category" 
-                        :items="items" 
-                        :rules="selectRules" 
-                        label="Kategorie" 
-                        variant="outlined" 
-                        density="compact" 
-                        prepend-inner-icon="mdi-tag" 
-                        class="mb-3"
-                        color="indigo-darken-2"
-                        required
-                        :disabled="loading">
-                    </v-select>
-                    
-                    <v-textarea 
-                        v-model="formData.message" 
-                        label="Deine Nachricht" 
-                        :rules="messageRules" 
-                        required
-                        variant="outlined" 
-                        density="compact" 
-                        prepend-inner-icon="mdi-message-text" 
-                        class="mb-3"
-                        color="indigo-darken-2"
-                        rows="3"
-                        counter="500"
-                        auto-grow
-                        :disabled="loading">
+
+                    <v-row>
+                        <v-col cols="12" md="6">
+                            <v-text-field v-model="formData.roomnumber" label="Zimmernummer (optional)"
+                                variant="outlined" density="compact" prepend-inner-icon="mdi-door" class="mb-3"
+                                color="indigo-darken-2" :disabled="loading">
+                            </v-text-field>
+                        </v-col>
+
+
+
+                        <v-col cols="12" md="6">
+                            <v-select v-model="formData.category" :items="items" :rules="selectRules" label="Kategorie"
+                                variant="outlined" density="compact" prepend-inner-icon="mdi-tag" class="mb-3"
+                                color="indigo-darken-2" required :disabled="loading">
+                            </v-select>
+                        </v-col>
+                    </v-row>
+
+
+                    <v-textarea v-model="formData.message" label="Deine Nachricht" :rules="messageRules" required
+                        variant="outlined" density="compact" prepend-inner-icon="mdi-message-text" class="mb-3"
+                        color="indigo-darken-2" rows="3" counter="500" auto-grow :disabled="loading">
                     </v-textarea>
                 </v-form>
             </v-card-text>
 
-                        <!-- Erfolg/Fehler Meldungen -->
-                        <v-alert
-                v-if="successMessage"
-                type="success"
-                variant="tonal"
-                class="mb-3"
-                closable
+            <!-- Erfolg/Fehler Meldungen -->
+            <v-alert v-if="successMessage" type="success" variant="tonal" class="mb-3" closable
                 @click:close="successMessage = ''">
                 {{ successMessage }}
             </v-alert>
-            
-            <v-alert
-                v-if="errorMessage"
-                type="error"
-                variant="tonal"
-                class="mb-3"
-                closable
+
+            <v-alert v-if="errorMessage" type="error" variant="tonal" class="mb-3" closable
                 @click:close="errorMessage = ''">
                 {{ errorMessage }}
             </v-alert>
-            
+
             <v-card-actions class="pa-0 pt-2">
-                <v-checkbox 
-                    v-if="!sucess"
-                    v-model="formData.anonymous" 
-                    label="Anonym bleiben" 
-                    color="indigo-darken-2"
-                    density="compact" 
-                    class="flex-grow-0"
-                    :disabled="loading">
+                <v-checkbox v-if="!sucess" v-model="formData.anonymous" label="Anonym bleiben" color="indigo-darken-2"
+                    density="compact" class="flex-grow-0" :disabled="loading">
                 </v-checkbox>
-                
+
                 <v-spacer></v-spacer>
-                
-                <v-btn 
-                    v-if="!sucess"
-                    color="indigo-darken-2" 
-                    variant="elevated" 
-                    size="small" 
-                    rounded="pill"
-                    class="px-4 font-weight-bold" 
-                    :disabled="!valid || loading"
-                    :loading="loading"
-                    @click="submitForm">
+
+                <v-btn v-if="!sucess" color="indigo-darken-2" variant="elevated" size="small" rounded="pill"
+                    class="px-4 font-weight-bold" :disabled="!valid || loading" :loading="loading" @click="submitForm">
                     <v-icon class="mr-1" size="small">mdi-send</v-icon>
                     Absenden
                 </v-btn>
 
-                <v-btn
-                    v-if="sucess"
-                    color="indigo-darken-2"
-                    variant="outlined"
-                    size="small"
-                    rounded="pill"
-                    class="px-4 font-weight-bold flex-grow-1 mr-2"
-                    @click="router.push('/info')">
+                <v-btn v-if="sucess" color="indigo-darken-2" variant="outlined" size="small" rounded="pill"
+                    class="px-4 font-weight-bold flex-grow-1 mr-2" @click="router.push('/info')">
                     <v-icon class="mr-1" size="small">mdi-arrow-left</v-icon>
                     Zurück
                 </v-btn>
-                <v-btn
-                    v-if="sucess"
-                    color="indigo-darken-2"
-                    variant="elevated"
-                    size="small"
-                    rounded="pill"
-                    class="px-4 font-weight-bold flex-grow-1"
-                    @click="router.push('/qr')">
+                <v-btn v-if="sucess" color="indigo-darken-2" variant="elevated" size="small" rounded="pill"
+                    class="px-4 font-weight-bold flex-grow-1" @click="router.push('/qr')">
                     Weiter
                     <v-icon class="ml-1" size="small">mdi-arrow-right</v-icon>
                 </v-btn>
@@ -177,14 +123,14 @@ export default {
                 this.errorMessage = '';
                 this.successMessage = '';
 
-                if(this.formData.anonymous) {
+                if (this.formData.anonymous) {
                     this.formData.anonymous = 'Ja';
                     this.formData.firstname = 'Anonym';
                     this.formData.lastname = 'Anonym';
                 } else {
                     this.formData.anonymous = 'Nein';
                 }
-                
+
                 try {
                     // EmailJS Template Variablen
                     const templateParams = {
@@ -197,7 +143,7 @@ export default {
                         date: new Date().toLocaleDateString('de-DE'),
                         time: new Date().toLocaleTimeString('de-DE')
                     };
-                    
+
                     // E-Mail über EmailJS senden
                     await emailjs.send(
                         'service_7u6xfpo',      //Service ID
@@ -205,15 +151,15 @@ export default {
                         templateParams,
                         'mhUHetv55RImbj_Mv'          //user ID
                     );
-                    
+
                     // Erfolgsmeldung
                     this.successMessage = 'Deine Nachricht wurde erfolgreich gesendet! Vielen Dank für dein Vertrauen.';
                     this.sucess = true;
-                    
+
                     // Formular zurücksetzen
                     this.$refs.form.reset();
                     this.select = null;
-                    
+
                 } catch (error) {
                     console.error('EmailJS Fehler:', error);
                     this.errorMessage = 'Fehler beim Senden der Nachricht. Bitte versuche es später erneut.';
